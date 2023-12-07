327024b92fa7bb5a04e1374809eb0514ba35aacc
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-12-07 15:32:10 +0000

    Deleting A specification as it seems to cause a conflict.

--- main.s
+++ main.s
@@ -19,62 +19,62 @@ initialise:
 	
 	; Specifying permitted inputs
 	movlw   permitted_inputs
-	movwf   FSR0, A ; Set FSR0 to point to the start of permitted_inputs
+	movwf   FSR0 ; Set FSR0 to point to the start of permitted_inputs
 
 	movlw   0x1
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x2
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x3
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x4
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x5
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x6
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x7
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x8
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0x9
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0xA
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0xB
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0xC
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0xD
-	movwf   INDF0, A
+	movwf   INDF0
 	incf    FSR0, F
 
 	movlw   0xE
-	movwf   INDF0, A
+	movwf   INDF0
 	
 	
 	; Specifying the maximum number allowed in each target slot (12 numbers)
@@ -92,7 +92,7 @@ initialise:
 	
 	; Setting the Character_Input function inputs
 	movlw	permitted_inputs ; First of the allowed memory values
-	movwf	input1, A
+	movwf	input1
 	
 	movlw	0x6 ; Length of allowed values from 0x30
 	movwf	input2
@@ -101,10 +101,10 @@ initialise:
 	movwf	input3
 	
 	movlw	0x0 ; Output displayed from the start of the LCD
-	movwf	input4, A
+	movwf	input4
 	
 	movlw	max_players
-	movwf	input5, A ; Storing in max_players memory location
+	movwf	input5 ; Storing in max_players memory location
 	
 	call	Character_Input
 	
@@ -112,9 +112,9 @@ initialise:
 	
 	; Random number generation
 	movlw	target_numbers ; First location of random numbers
-	movwf	input1, A
+	movwf	input1
 	movf	target_size, W
-	movwf	input2, A
+	movwf	input2
 	call	RNG
 
 first_player_turn:
@@ -128,19 +128,19 @@ player_turn:
 	
 	; Input guess
 	movlw	permitted_inputs ; First of the allowed memory values
-	movwf	input1, A
+	movwf	input1
 	
 	movlw	0x12 ; 12 allowed values
-	movwf	input2, A
+	movwf	input2
 	
 	movlw	0x4 ; Maximum number of input characters allowed
-	movwf	input3, A
+	movwf	input3
 	
 	movlw	0x0 ; Output should be displayed at the bottom row of the LCD
-	movwf	input4, A
+	movwf	input4
 	
 	movlw	guess_array
-	movwf	input5, A ; Storing in max_players memory location
+	movwf	input5 ; Storing in max_players memory location
 	
 	call	Character_Input
 	
@@ -155,7 +155,7 @@ player_turn:
 	movwf	input3
 	
 	call	Number_Correct
-	movwf	number_correct, A
+	movwf	number_correct
 	
 	; Check if all of the guess is correct
 	subwf	target_size, W
@@ -188,237 +188,237 @@ end_game:
 
 Input_Player_Msg:
     movlw   lcd_msg
-    movwf   FSR0, A
+    movwf   FSR0
     
     movlw   'N'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'o'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   '.'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'O'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'f'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'P'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'l'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'a'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'y'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'e'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   's'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ':'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     
     return
 
 Player_Turn_Msg:
     movlw   lcd_msg
-    movwf   FSR0, A
+    movwf   FSR0
     
     movlw   'P'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'l'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'a'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'y'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'e'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movf    player_num, W
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'T'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'u'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'n'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     
     return
     
     
 Number_Correct_Msg:
     movlw   lcd_msg
-    movwf   FSR0, A
+    movwf   FSR0
     
     movlw   'N'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'o'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   '.'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'C'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'o'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movf    player_num, W
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'e'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'c'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   't'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ':'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movf    number_correct, W
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     
     return
     
 Winner_Msg:
     movlw   lcd_msg
-    movwf   FSR0, A
+    movwf   FSR0
     
     movlw   'W'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'i'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'n'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'n'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'e'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ':'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'P'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'l'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'a'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'y'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'e'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   'r'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   ' '
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   '1'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     movlw   '!'
-    movwf   INDF0, A
-    incf    FSR0, A
+    movwf   INDF0
+    incf    FSR0
     
     return
     
